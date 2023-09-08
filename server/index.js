13f18bec718af82c6a058a8d007393a1b9f78b01
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-08 19:56:19 +0330

    add packages and start server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+
+});
+
