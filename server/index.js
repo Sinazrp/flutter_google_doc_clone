cec693d3453af249949afda0d33391599ab06649
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-08 20:24:03 +0330

    connected to mongo

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
+const DB = "mongodb://root:Wkumle3XWuZSxgfIolzS3Eup@aberama.iran.liara.ir:32074/my-app?authSource=admin";
+mongoose.connect(DB).then(() => { console.log("DB Connected"); }).catch((err) => { console.log(err); })
+
+
 
 const app = express();
 
