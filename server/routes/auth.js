0e969f08ceac1223be717c38276a2f19d436328b
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-09 22:59:13 +0330

    fix user export

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,6 +5,9 @@ const authRouter = express.Router();
 authRouter.post('/api/signup', async (req, res) => {
     try {
         const { name, email, profilePic } = req.body;
+
+
+
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
     }
