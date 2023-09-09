d90b9d257604a2905efbddb09e8dc384ac6ccc20
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-10 00:24:16 +0330

    create api

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT || 3001;
+
 const DB = "mongodb://root:Wkumle3XWuZSxgfIolzS3Eup@aberama.iran.liara.ir:32074/my-app?authSource=admin&authMechanism=SCRAM-SHA-256";
 mongoose.connect(DB).then(() => { console.log("DB Connected"); }).catch((err) => { console.log(err); })
 
 
 
+
 const app = express();
+app.use(express.json());
+app.use(authRouter);
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const User = require('../model/user');
 
 const authRouter = express.Router();
 
 authRouter.post('/api/signup', async (req, res) => {
     try {
         const { name, email, profilePic } = req.body;
+        let user = await User.findOne({ email: email });
+        if (!user) {
+            user = new User({
+                name,
+                email,
+                profilePic
+            });
+            user = await user.save();
 
+            res.status(201).json({ user });
 
-
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ error });
     }
 
 }
-)
+);
+module.exports = authRouter;
