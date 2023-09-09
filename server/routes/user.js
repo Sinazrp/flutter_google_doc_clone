54c567582dba66728a11a573b04d916259579319
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-09 22:30:13 +0330

    Update user.js

---
+++ server/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const authRouter = express.Router();
+
+authRouter.post('/api/signup', async (req, res) => {
+    try {
+        const { name, email, profilePic } = req.body;
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+
+}
+)
