56977d1fdbec52cc960c2a908c2d6fcd73160ae5
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-10 18:54:00 +0330

    add client request

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,21 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_google_doc_clone/consts.dart';
+import 'package:flutter_google_doc_clone/model/user_model.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authRepositoryProvider =
-    Provider((ref) => AuthRepository(googleSignIn: GoogleSignIn()));
+    Provider((ref) => AuthRepository(googleSignIn: GoogleSignIn(), dio: Dio()));
 
 class AuthRepository {
+  final Dio _dio;
   final GoogleSignIn _googleSignIn;
-  AuthRepository({
-    required GoogleSignIn googleSignIn,
-  }) : _googleSignIn = googleSignIn;
+  AuthRepository({required GoogleSignIn googleSignIn, required Dio dio})
+      : _googleSignIn = googleSignIn,
+        _dio = dio;
 
   void signIn() async {
     try {
       final user = await _googleSignIn.signIn();
       if (user != null) {
-        print(user.email);
-        print(user.displayName);
+        final userr = UserModel(
+            name: user.displayName!,
+            email: user.email,
+            profilePic: user.photoUrl!,
+            uid: '',
+            token: '');
+        var res = await _dio.post(
+          '$host/api/signin',
+          data: userr.toJson(),
+        );
       }
     } catch (e) {
       print(e);
