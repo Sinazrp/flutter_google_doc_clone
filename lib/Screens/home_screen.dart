fcaa3ed42e0a607937c8377311a5258abba51374
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-15 19:10:28 +0330

    Update home_screen.dart

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_google_doc_clone/repository/auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'login_screen.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
