41ff6a86f5faebf6ce8d17174eb8fbd34a643282
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-15 19:10:25 +0330

    Update auth_repository.dart

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,15 +1,11 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-import 'package:flutter_google_doc_clone/consts.dart';
 import 'package:flutter_google_doc_clone/model/error_model.dart';
 import 'package:flutter_google_doc_clone/model/user_model.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 final authRepositoryProvider =
     Provider((ref) => AuthRepository(googleSignIn: GoogleSignIn(), dio: Dio()));
-final userProvider = StateProvider<UserModel?>((ref) => null);
 
 class AuthRepository {
   final Dio _dio;
@@ -20,30 +16,42 @@ class AuthRepository {
 
   Future<ErrorModel> signIn() async {
     ErrorModel errorModel = ErrorModel(error: 'some Error happens', data: null);
-    try {
-      final user = await _googleSignIn.signIn();
-      if (user != null) {
-        final userr = UserModel(
-            name: user.displayName!,
-            email: user.email,
-            profilePic: user.photoUrl!,
-            uid: '',
-            token: '');
-        var res = await _dio.post(
-          '$host/api/signin',
-          data: userr.toJson(),
-        );
-        switch (res.statusCode) {
-          case 200:
-            final newUser =
-                userr.copyWith(uid: jsonDecode(res.data)['user']['_id']);
-            errorModel = ErrorModel(error: null, data: newUser);
-            break;
-        }
+
+    final user = await _googleSignIn.signIn();
+    print(user);
+    if (user != null) {
+      final userr = UserModel(
+          name: user.displayName.toString(),
+          email: user.email,
+          profilePic: user.photoUrl ?? 'gffgjkgy',
+          uid: '',
+          token: '');
+      print(userr);
+
+      var res = await _dio.post(
+        'https://doc-clone.iran.liara.run/api/signup',
+        data: {
+          "name": userr.name,
+          "email": userr.email,
+          "profilePic": userr.profilePic
+        },
+      );
+      switch (res.statusCode) {
+        case 201:
+          print(res.data);
+          final newUser = userr.copyWith(uid: res.data["user"]["_id"]);
+          print(newUser);
+          errorModel = ErrorModel(error: null, data: newUser);
+        case 200:
+          print(res.data);
+          final oldUser = UserModel.fromMap(res.data["user"]);
+          print(oldUser.toString());
+          errorModel = ErrorModel(error: null, data: oldUser);
+
+          break;
       }
-    } catch (e) {
-      errorModel = ErrorModel(error: e.toString(), data: null);
     }
+
     return errorModel;
   }
 }
