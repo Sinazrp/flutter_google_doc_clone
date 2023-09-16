6837ec2679e128548f398edbac86a42955b9f0e4
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-16 09:33:50 +0330

    Create local_storage_repo.dart

---
+++ lib/repository/local_storage_repo.dart
@@ -0,0 +1,8 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorageRepository {
+  void setToken(String token) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setString('x-auth-token', token);
+  }
+}
