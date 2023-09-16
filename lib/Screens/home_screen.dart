6820c46aaa0b26af59616f254fd01d6c88710ca5
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-16 22:22:31 +0330

    Update home_screen.dart

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_doc_clone/model/error_model.dart';
 import 'package:flutter_google_doc_clone/repository/auth_repository.dart';
+import 'package:flutter_google_doc_clone/repository/document_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 import 'login_screen.dart';
 
@@ -9,6 +12,19 @@ class HomeScreen extends ConsumerWidget {
   void signOut(WidgetRef ref) {
     ref.read(authRepositoryProvider).logOut();
     ref.read(userProvider.notifier).update((state) => null);
+  }
+
+  void createDocument(WidgetRef ref, BuildContext context) async {
+    final navigator = Routemaster.of(context);
+    final snackBar = ScaffoldMessenger.of(context);
+    ErrorModel errorModel =
+        await ref.read(docRepositoryProvider).createDocument();
+
+    if (errorModel.data != null) {
+      navigator.push('/document/${errorModel.data.id}');
+    } else {
+      snackBar.showSnackBar(SnackBar(content: Text(errorModel.error!)));
+    }
   }
 
   @override
