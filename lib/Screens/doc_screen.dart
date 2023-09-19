c2000932cdca897b20795fe96ff0a79c13b0d478
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-19 22:02:11 +0330

    Update doc_screen.dart

--- lib/Screens/doc_screen.dart
+++ lib/Screens/doc_screen.dart
@@ -15,12 +15,21 @@ class DocScreen extends ConsumerStatefulWidget {
 
 class _DocScreenState extends ConsumerState<DocScreen> {
   @override
+  void dispose() {
+    titleController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  TextEditingController titleController =
+      TextEditingController(text: 'untitled documents');
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           Padding(
-            padding: const EdgeInsets.all(11),
+            padding: const EdgeInsets.only(right: 8.0),
             child: ElevatedButton.icon(
               onPressed: () {},
               icon: const Icon(
@@ -33,11 +42,45 @@ class _DocScreenState extends ConsumerState<DocScreen> {
                 style: TextStyle(color: kWhiteColor),
               ),
               style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 6,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
                 backgroundColor: Colors.lightBlue,
               ),
             ),
           )
         ],
+        title: Row(children: [
+          Image.asset(
+            'assets/logo/docs-logo.png',
+            height: 30,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: TextField(
+              controller: titleController,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: OutlineInputBorder(),
+                contentPadding: EdgeInsets.only(left: 10),
+              ),
+              onTapOutside: (_) =>
+                  FocusManager.instance.primaryFocus?.unfocus(),
+            ),
+          )
+        ]),
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 229, 227, 227))),
+            )),
       ),
       body: Center(
         child: Text(widget.id),
