55c0ebecc505dc318c3f5299614124e93701df8b
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-19 21:15:59 +0330

    Update doc_screen.dart

--- lib/Screens/doc_screen.dart
+++ lib/Screens/doc_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_doc_clone/constans.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DocScreen extends ConsumerStatefulWidget {
@@ -16,6 +17,28 @@ class _DocScreenState extends ConsumerState<DocScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(11),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.lock,
+                size: 16,
+                color: kWhiteColor,
+              ),
+              label: const Text(
+                'share',
+                style: TextStyle(color: kWhiteColor),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightBlue,
+              ),
+            ),
+          )
+        ],
+      ),
       body: Center(
         child: Text(widget.id),
       ),
