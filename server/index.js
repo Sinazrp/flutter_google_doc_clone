2c67f0bbd1e576f5901e418fb74dfafdd1010e08
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-22 16:49:24 +0330

    maintaining the socket io

--- server/index.js
+++ server/index.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
 const authRouter = require('./routes/auth');
 const documentRouter = require('./routes/document');
 
 const PORT = process.env.PORT || 3001;
 
 const DB = "mongodb://root:Wkumle3XWuZSxgfIolzS3Eup@aberama.iran.liara.ir:32074/my-app?authSource=admin&authMechanism=SCRAM-SHA-256";
-mongoose.connect(DB).then(() => { console.log("DB Connected"); }).catch((err) => { console.log(err); })
+
 
 
 
 
 const app = express();
+var server = http.createServer(app);
+/* var socket = require('socket.io');
+var io = socket(server); */
+// shorter way
+var socket = require('socket.io')(server);
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-
-
 app.use(documentRouter);
 app.use(authRouter);
+
+
+
+mongoose.connect(DB).then(() => { console.log("DB Connected"); }).catch((err) => { console.log(err); });
+
+io.on('connection', (socket) => {
+    console.log("Connected" + socket.id);
+
+})
 
 
 app.listen(PORT, "0.0.0.0", () => {
