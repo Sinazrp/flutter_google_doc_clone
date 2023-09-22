6fa6d4102afef756f06895ff66cb1269d1550da3
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-22 16:01:38 +0330

    Create socket_client.dart

---
+++ lib/client/socket_client.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_google_doc_clone/consts.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+class SocketClient {
+  io.Socket? socket;
+  static SocketClient? _instance;
+  SocketClient._internal() {
+    socket = io.io(host, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false
+    });
+    socket!.connect();
+  }
+  static SocketClient? get instance {
+    _instance ??= SocketClient._internal();
+    return _instance;
+  }
+}
