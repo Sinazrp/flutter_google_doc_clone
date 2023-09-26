75f1d656db1263e82209e4f16371ad5822c41552
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-09-26 21:50:09 +0330

    Update socket_repository.dart

--- lib/repository/socket_repository.dart
+++ lib/repository/socket_repository.dart
@@ -4,4 +4,7 @@ import 'package:socket_io_client/socket_io_client.dart' as io;
 class SocketRepsitory {
   final _socketClient = SocketClient.instance.socket!;
   io.Socket get socketClient => _socketClient;
+  void joinRoom(String documentId) {
+    _socketClient.emit('join', documentId);
+  }
 }
